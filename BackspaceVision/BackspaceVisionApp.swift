9561d753654770b4144a00ab68e749e9ea6e24a5
Faustharus <chailloleau-damien@outlook.fr>
2024-10-16 10:31:03 +0200

    Initial Commit

---
+++ BackspaceVision/BackspaceVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackspaceVisionApp.swift
+//  BackspaceVision
+//
+//  Created by Damien Chailloleau on 16/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct BackspaceVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
